810a3218e0058a4f5e1ba93755043625bddf03e0
victoria <vjohkv@gmail.com>
2019-05-17 12:12:34 -0700

    Fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'sanrio.js',
-        // path: path.resolve(__dirname, 'dist')
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
     },
     devtool: 'inline-source-map'
 };
