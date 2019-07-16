3ff2290f32ec1b4837ea8ab0034a29d22b11ffaa
victoria <vjohkv@gmail.com>
2019-07-16 10:51:11 -0700

    Configure keyboard layout (minus backspace)

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -25,41 +25,122 @@ export default class Menu {
 
     generateControls(){
         let keyboard = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+        let nums = '1234567890';
         let number = 0;
         let letter = 0;
         let keyArray = [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0],
-            [2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0],
-            [2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            [4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4],
+            // numbers
+            [5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 5],
+            // qwertyuiop
+            [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 5, 5, 0],
+            // asdfghjkl
+            [8, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 5, 5, 5],
+            // zxcvbnm
+            [6, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 6],
+            // space
+            [0, 0, 0, 0, 7, 0, 0, 9, 10, 11]
         ];
 
-        for (let row = 1; row < keyArray.length; row++){
+        for (let row = 0; row < keyArray.length; row++){
             let list = document.getElementById(`row${row}`);
             keyArray[row].forEach(key => {
                 let element = document.createElement('LI');
                 switch (key){
-                    case 0:
-                        element.setAttribute("class", "keyboardKey");
-                        list.appendChild(element);
-                        break;
                     case 1:
                         element.setAttribute("class", "keyboardKey");
-                        let textnode = document.createTextNode(`${number}`);
-                        // textnode.setAttribute("id", `key${number}`);
+                        element.setAttribute("id", `key${nums[number]}`);
+                        let textnode = document.createElement('P');
+                        let textContent = document.createTextNode(`${nums[number]}`);
+                        textnode.setAttribute("class", "keyNumberContent");
+                        textnode.appendChild(textContent);
                         element.appendChild(textnode);
                         list.appendChild(element);
                         number += 1;
                         break;
                     case 2:
                         element.setAttribute("class", "keyboardKey");
-                        let letternode = document.createTextNode(`${keyboard[letter]}`);
-                        // letternode.setAttribute("id", `key${keyboard[letter]}`);
+                        element.setAttribute("id", `key${keyboard[letter]}`);
+                        let letternode = document.createElement('P');
+                        let letterContent = document.createTextNode(`${keyboard[letter]}`);
+                        letternode.setAttribute("class", "keyLetterContent");
+                        letternode.appendChild(letterContent);
                         element.appendChild(letternode);
                         list.appendChild(element);
                         letter += 1;
+                        break;
+                    case 7:
+                        element.setAttribute("id", "space");
+                        list.appendChild(element);
+                        break;
+                    // arrow keys
+                    case 9:
+                        element.setAttribute("class", "arrowContainer");
+                        let leftArrow = document.createElement('DIV');
+                        leftArrow.setAttribute("class", "arrow");
+                        leftArrow.setAttribute("id", "leftArrow");
+
+                        let blank = document.createElement('DIV');
+                        blank.setAttribute("class", "arrow");
+
+                        element.appendChild(blank);
+                        element.appendChild(leftArrow);
+                        list.appendChild(element);
+                        break;  
+                    case 10:
+                        element.setAttribute("class", "arrowContainer");
+                        let upArrow = document.createElement('DIV');
+                        upArrow.setAttribute("class", "arrow");
+                        upArrow.setAttribute("id", "upArrow");
+
+                        let downArrow = document.createElement('DIV');
+                        downArrow.setAttribute("class", "arrow");
+                        downArrow.setAttribute("id", "downArrow");
+
+                        element.appendChild(upArrow);
+                        element.appendChild(downArrow);
+                        list.appendChild(element);
+                        break;  
+                    case 11:
+                        element.setAttribute("class", "arrowContainer");
+                        
+                        let rightArrow = document.createElement('DIV');
+                        rightArrow.setAttribute("class", "arrow");
+                        rightArrow.setAttribute("id", "rightArrow");
+
+
+                        let rightblank = document.createElement('DIV');
+                        rightblank.setAttribute("class", "arrow");
+
+                        element.appendChild(rightblank);
+                        element.appendChild(rightArrow);
+                        list.appendChild(element);
+                        break;  
+
+                    // blank keys
+                    case 0:
+                        element.setAttribute("class", "keyboardKey");
+                        list.appendChild(element);
+                        break;
+                    case 3:
+                        element.setAttribute("class", "quarterKey");
+                        list.appendChild(element);
+                        break;
+                    case 4:
+                        element.setAttribute("class", "halfKey");
+                        list.appendChild(element);
+                        break;
+                    case 5:
+                        element.setAttribute("class", "verticalHalfKey");
+                        list.appendChild(element);
+                        break;
+                    case 6:
+                        element.setAttribute("class", "shift");
+                        list.appendChild(element);
+                        break;
+                    case 8:
+                        element.setAttribute("class", "caps");
+                        list.appendChild(element);
                         break;
                     default:
                         break;
