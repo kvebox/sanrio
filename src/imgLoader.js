7aacf5eb7e7d71f8e1680e5ab6d123845ebb9b80
victoria <vjohkv@gmail.com>
2019-07-22 21:57:24 -0700

    Consolidate images files

--- src/imgLoader.js
+++ src/imgLoader.js
@@ -1,6 +1,16 @@
-// import menu_arrow from './images/menu/menuCursor.png';
+// menu
+const menuArrowImg = new Image();
+menuArrowImg.src = './src/images/menu/life.png';
 
-// const menuArrowImg = new Image();
-// menuArrowImg.src = menu_arrow;
+export const menuArrow = [menuArrowImg];
 
-// export const menuArrow = [menuArrowImg];
+// scene
+const platformLeftImg = new Image();
+const platformMidImg = new Image();
+const platformRightImg = new Image();
+
+platformLeftImg.src = './src/images/scene/platformLeft_olive.png';
+platformMidImg.src = './src/images/scene/platformMiddle_olive.png';
+platformRightImg.src = './src/images/scene/platformRight_olive.png';
+
+export const platformOlive = [platformLeftImg, platformMidImg, platformRightImg];
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.txt$/i,
-                use: 'raw-loader',
-            }
-        ]
-    },
     entry: './src/index.js',
     output: {
         filename: 'sanrio.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.txt$/i,
+            use: 'raw-loader',
+        },
+        {
+            test: /\.(png|jpe?g|gif)$/,
+            use: 'file-loader'
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.css', '.txt', '.png', '*']
     },
     devtool: 'inline-source-map'
 };
