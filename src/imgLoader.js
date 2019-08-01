99fc24f29b985e39eaa4c0bf67f5bed90293b351
victoria <vjohkv@gmail.com>
2019-08-01 00:44:41 -0700

    Create scene image files

--- src/imgLoader.js
+++ src/imgLoader.js
@@ -5,12 +5,18 @@ menuArrowImg.src = './src/images/menu/life.png';
 export const menuArrow = [menuArrowImg];
 
 // scene
-const platformLeftImg = new Image();
-const platformMidImg = new Image();
-const platformRightImg = new Image();
+const platformLeftImg_3 = new Image();
+const platformMidImg_3 = new Image();
+const platformRightImg_3 = new Image();
 
-platformLeftImg.src = './src/images/scene/platformLeft_olive.png';
-platformMidImg.src = './src/images/scene/platformMiddle_olive.png';
-platformRightImg.src = './src/images/scene/platformRight_olive.png';
+platformLeftImg_3.src = './src/images/scene/platformLeft_3.png';
+platformMidImg_3.src = './src/images/scene/platformMiddle_3.png';
+platformRightImg_3.src = './src/images/scene/platformRight_3.png';
 
-export const platformOlive = [platformLeftImg, platformMidImg, platformRightImg];
+
+
+export const platformOlive = [platformLeftImg_3, platformMidImg_3, platformRightImg_3];
+export const bushes = [];
+export const clouds = [];
+export const trees = [];
+export const hills = [];
