d4bff6a5e01f6e1300a0bed1e21637144d258531
victoria <vjohkv@gmail.com>
2019-08-09 03:22:53 -0700

    Create bush objects

--- src/scene/bush.js
+++ src/scene/bush.js
@@ -2,14 +2,48 @@ import StaticObject from './staticObject';
 import { bushes } from '../imgLoader';
 
 export default class Bush extends StaticObject {
-    constructor(x,y){
+    constructor(x,y, number){
         super(x,y);
-        this.image = '';
+        this.number = number;
+        this.image = bushes[this.number];
 
         this.position = {
             x: x,
             y: y
         };
 
+        this.create();
     }
+
+    create() {
+        let multiplier = this.randomInt(2, 4);
+        switch (this.number) {
+            case 0:
+                this.height = 102 / multiplier;
+                this.width = 359 / multiplier;
+                break;
+            case 1:
+                this.height = 75 / multiplier;
+                this.width = 238 / multiplier;
+                break;
+            case 2:
+                this.height = 96 / multiplier;
+                this.width = 385 / multiplier;
+                break;
+            case 3:
+                this.height = 80 / multiplier;
+                this.width = 297 / multiplier;
+                break;
+            case 4:
+                this.height = 113 / multiplier;
+                this.width = 290 / multiplier;
+                break;
+            case 5:
+                this.height = 74 / multiplier;
+                this.width = 208 / multiplier;
+                break;
+
+        }
+    }
+    
 }
